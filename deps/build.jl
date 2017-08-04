4ef3bcebeec1691d3cc41d23057b1f28e6dcda9c
Simon Byrne <simonbyrne@gmail.com>
2017-08-04 14:06:26 -0700

    use MAKE_CMD

--- deps/build.jl
+++ deps/build.jl
@@ -18,9 +18,6 @@ provides(Sources, URI("https://github.com/JuliaLang/Rmath-julia/archive/v$versio
 prefix = joinpath(BinDeps.depsdir(libRmath), "usr")
 srcdir = joinpath(BinDeps.srcdir(libRmath), "Rmath-julia-$version")
 
-# These Windows binaries were taken from `make -C deps install-Rmath-julia`
-# in a Cygwin cross-compile from the release-0.4 branch of julia
-# Future work: standalone cross-compiled binaries using openSUSE docker container
 
 provides(Binaries,
     URI("https://github.com/JuliaLang/Rmath-julia/releases/download/v$version/libRmath-julia-win-$(Sys.ARCH)-v$version.zip"),
@@ -29,19 +26,15 @@ provides(Binaries,
                               "87e46273e503a9c5b65cf8d920006cb0771d24e38a1c2181e91c84a07cd27f58" ,
     os = :Windows)
 
-# BSD systems (other than macOS) use BSD Make rather than GNU Make by default
-# We need GNU Make, and on such systems GNU make is invoked as `gmake`
-make = is_bsd() && !is_apple() ? "gmake" : "make"
 
-# If your library uses configure or cmake, good idea to do an
-# out-of-tree build - see examples in JuliaOpt and JuliaWeb
+
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(libRmath)
         CreateDirectory(joinpath(prefix, "lib"))
         @build_steps begin
             ChangeDirectory(srcdir)
-            `$make`
+            MAKE_CMD
             `mv src/libRmath-julia.$(Libdl.dlext) "$prefix/lib"`
         end
     end), [libRmath], os = :Unix)
